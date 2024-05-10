a0c890a3b1ecd67649993abb00d35d0634d014a8
Bradley Fisher <bradley.fisher@paylinksolutions.co.uk>
2024-05-10 09:17:51 +0100

    installed bootstrap 5 as a dependency to style the form out the box - also added an appConfig to stop console error being thrown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormioModule } from '@formio/angular';
+import { FormioAppConfig, FormioModule } from '@formio/angular';
+import { AppConfig } from './forms-io.config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FormioModule],
+  providers: [{ provide: FormioAppConfig, useValue: AppConfig }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
