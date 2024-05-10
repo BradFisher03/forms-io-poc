dff2b1c974f4416457ef149a0f1147d448907bbd
Bradley Fisher <bradley.fisher@paylinksolutions.co.uk>
2024-05-10 08:59:47 +0100

    Added the forms-io example URL as it's all I can reach for now, no styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormioModule } from '@formio/angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormioModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'forms-io-poc';
-}
+export class AppComponent {}
